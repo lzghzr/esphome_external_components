e541eb2afb689ed696ae1a1c970a27db9b3ff0e1
lzghzr <me@lzzr.me>
2026-06-05 21:19:46 +0800

    适配 esphome 2026.04

--- external_components/remote_transmitter/remote_transmitter.h
+++ external_components/remote_transmitter/remote_transmitter.h
@@ -136,7 +136,7 @@ class RemoteTransmitterComponent : public remote_base::RemoteTransmitterBase,
   rmt_channel_handle_t channel_{NULL};
   rmt_encoder_handle_t encoder_{NULL};
   esp_err_t error_code_{ESP_OK};
-  std::string error_string_{""};
+  std::string error_string_;
   bool inverted_{false};
   bool non_blocking_{false};
   gpio_num_t pin_num = gpio_num_t(this->pin_->get_pin());
